afb7a7b1d20dd5cbbbcbc0b04881b28534ccdbf8
UntouchedWagons <jordan.w.bradley@gmail.com>
2015-09-23 12:46:02 -0400

    Converted CraftStudio models to Sketchup+Obj

--- src/main/java/untouchedwagons/minecraft/powerlines/tileentity/TileEntityMediumPowerLine.java
+++ src/main/java/untouchedwagons/minecraft/powerlines/tileentity/TileEntityMediumPowerLine.java
@@ -1,7 +1,5 @@
 package untouchedwagons.minecraft.powerlines.tileentity;
 
-import untouchedwagons.minecraft.powerlines.extra.ConnectionPointCoordinateCalculator;
-
 public class TileEntityMediumPowerLine extends TileEntityPowerGridNode {
     public TileEntityMediumPowerLine() {
         super();
